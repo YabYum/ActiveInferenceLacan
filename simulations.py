015b0163a317486d8d1ec09f6805613c1ab2de4f
LingyuLi <lilingyu1874@sjtu.edu.cn>
2024-07-05 13:38:50 +0800

    Three

--- simulations.py
+++ simulations.py
@@ -1,9 +1,13 @@
-from agent import Subject, Sync
+from agent import Subject, Sync, Three
 
 subj = Subject()
 subj.run_active_inference(15)
 subj.plot_trajectory()
 
 sync = Sync()
-sync.run_active_inference(5)
+sync.run_active_inference(15)
 sync.plot_trajectories()
+
+three = Three()
+three.run_active_inference(15)
+three.plot_trajectories()
